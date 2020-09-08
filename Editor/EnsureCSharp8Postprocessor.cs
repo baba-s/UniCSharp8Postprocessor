30fab31c707c14b0ef4ccaf0d063fbe3773e432e
baba_s <shotababa1206@gmail.com>
2020-09-08 20:10:16 +0900

    first commit

---
+++ Editor/EnsureCSharp8Postprocessor.cs
@@ -0,0 +1,42 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using UnityEditor;
+
+namespace Kogane.Internal
+{
+	// https://forum.unity.com/threads/unity-c-8-support.663757/page-4#post-5963279
+	internal sealed class EnsureCSharp8Postprocessor : AssetPostprocessor
+	{
+		private static string OnGeneratedCSProject( string path, string content )
+		{
+			var document = XDocument.Parse( content );
+
+			var root = document.Root;
+			if ( root is null ) throw new ArgumentNullException( nameof( root ) );
+
+			var ns = root.GetDefaultNamespace();
+
+			var element = root.Descendants( ns + "LangVersion" ).SingleOrDefault();
+			if ( element == null ) throw new ArgumentNullException( nameof( element ) );
+
+			element.Value = "8.0";
+
+			using ( var writer = new Utf8StringWriter() )
+			{
+				document.Save( writer );
+
+				content = writer.ToString();
+			}
+
+			return content;
+		}
+
+		private sealed class Utf8StringWriter : StringWriter
+		{
+			public override Encoding Encoding => Encoding.UTF8;
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# UniCSharp8Postprocessor
